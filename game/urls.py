189d6ff6208d14cb6d1fac2a8e5d6e0e8bc2b3cc
Shubham <shubhams2m@gmail.com>
2019-12-15 22:28:42 +0530

    Removed some redundant URLs

--- game/urls.py
+++ game/urls.py
@@ -20,7 +20,6 @@ from registration.views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('logout/',logoutView, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('registration/', include('registration.urls')),
